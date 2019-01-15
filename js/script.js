16ca31f58e625206c91de1230c57071f9b376b12
Vivendum <vivendum@mail.ru>
2019-01-15 16:24:17 +0500

    Меняет фон боди при переключении слайдера

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
+var body_color_1 = document.querySelector("#advertising-product-1");
+var body_color_2 = document.querySelector("#advertising-product-2");
+var body_color_3 = document.querySelector("#advertising-product-3");
+var body = document.querySelector("body");
 var open_popup_connection = document.querySelector(".button-connection");
 var popup_connection = document.querySelector(".modal-connection");
 var overlay = document.querySelector(".overlay");
 var focus_connection_name = popup_connection.querySelector("[name=name]");
 var close_popup_connection = popup_connection.querySelector(".modal-close");
+
+body_color_1.addEventListener("focus", function (evt) {
+  body.classList.add("js-color-1");
+  body.classList.remove("js-color-2");
+  body.classList.remove("js-color-3");
+});
+
+body_color_2.addEventListener("focus", function (evt) {
+  body.classList.add("js-color-2");
+  body.classList.remove("js-color-1");
+  body.classList.remove("js-color-3");
+});
+
+body_color_3.addEventListener("focus", function (evt) {
+  body.classList.add("js-color-3");
+  body.classList.remove("js-color-1");
+  body.classList.remove("js-color-2");
+});
 
 open_popup_connection.addEventListener("click", function (evt) {
   evt.preventDefault();
