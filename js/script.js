386256d91604df8adba425ebdba62046cd763966
Vivendum <vivendum@mail.ru>
2019-01-15 15:50:16 +0500

    Удаляет неправильный код для таб-навигации по модальному окну

--- js/script.js
+++ js/script.js
@@ -9,25 +9,10 @@ open_popup_connection.addEventListener("click", function (evt) {
   popup_connection.classList.add("js-modal-open");
   overlay.classList.add("js-modal-open");
   focus_connection_name.focus();
-  // close_popup_connection.addEventListener("focus", function () {
-    // window.addEventListener("keydown", function (evt) {
-      // if (evt.keyCode === 9) {
-        // focus_connection_name.focus();
-      // }
-    // });
-  // });
 });
 
 close_popup_connection.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup_connection.classList.remove("js-modal-open");
   overlay.classList.remove("js-modal-open");
-});
-
-close_popup_connection.addEventListener("focus", function () {
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 9) {
-      focus_connection_name.focus();
-    }
-  });
 });
