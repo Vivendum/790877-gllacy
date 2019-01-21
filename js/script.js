45c3282a1a6f898aa0b64e7f7d4c2a67b6c82951
Vivendum <vivendum@mail.ru>
2019-01-21 08:38:49 +0500

    Исправляет ошибку в js

--- js/script.js
+++ js/script.js
@@ -11,12 +11,21 @@ var name_form_popup_connection = form_popup_connection.querySelector("[name=name
 var email_form_popup_connection = form_popup_connection.querySelector("[name=email]");
 var comment_form_popup_connection = form_popup_connection.querySelector("[name=comment]");
 var button_form_popup_connection = form_popup_connection.querySelector(".button");
-var is_storage_support = true;
+// var storage_name = localStorage ? localStorage.getItem("name") : null; // Зап
 var storage_name = localStorage.getItem("name");
+// var storage_email = localStorage ? localStorage.getItem("email") : null;
 var storage_email = localStorage.getItem("email");
+var is_storage_support = true;
 
 try {
   storage_name = localStorage.getItem("name");
+  // storage_email = localStorage.getItem("email");
+} catch (err) {
+  is_storage_support = false;
+};
+
+try {
+  // storage_name = localStorage.getItem("name");
   storage_email = localStorage.getItem("email");
 } catch (err) {
   is_storage_support = false;
@@ -52,6 +61,21 @@ open_popup_connection.addEventListener("click", function (evt) {
   } else {
       name_form_popup_connection.focus();
     };
+  form_popup_connection.addEventListener("submit", function (evt) {
+    if (!name_form_popup_connection.value || !email_form_popup_connection.value || !comment_form_popup_connection.value) {
+      evt.preventDefault();
+      popup_connection.classList.add("js-animation-modal-error");
+    } else {
+      localStorage.setItem("name", name_form_popup_connection.value);
+      localStorage.setItem("email", email_form_popup_connection.value);
+      overlay.classList.remove("js-modal-open");
+      popup_connection.classList.remove("js-modal-open");
+      popup_connection.classList.remove("js-animation-modal-open");
+    };
+  });
+  button_form_popup_connection.addEventListener("click", function () {
+    popup_connection.classList.remove("js-animation-modal-error");
+  });
   close_popup_connection.addEventListener("click", function (evt) {
     evt.preventDefault();
     overlay.classList.remove("js-modal-open");
@@ -67,20 +91,5 @@ open_popup_connection.addEventListener("click", function (evt) {
       popup_connection.classList.remove("js-animation-modal-open");
       popup_connection.classList.remove("js-animation-modal-error");
     };
-  });
-  form_popup_connection.addEventListener("submit", function (evt) {
-    if (!name_form_popup_connection.value || !email_form_popup_connection.value || !comment_form_popup_connection.value) {
-      evt.preventDefault();
-      popup_connection.classList.add("js-animation-modal-error");
-    } else {
-      localStorage.setItem("name", name_form_popup_connection.value);
-      localStorage.setItem("email", email_form_popup_connection.value);
-      overlay.classList.remove("js-modal-open");
-      popup_connection.classList.remove("js-modal-open");
-      popup_connection.classList.remove("js-animation-modal-open");
-    };
-    button_form_popup_connection.addEventListener("click", function () {
-      popup_connection.classList.remove("js-animation-modal-error");
-    });
   });
 });
