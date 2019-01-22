7cbe036ce5fa215452a26fa90b4270f79f7f02ea
Vivendum <vivendum@mail.ru>
2019-01-22 14:43:56 +0500

    Улучшает CSS и JS код для работы в IE. Минимизирует

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-var body_color_1 = document.querySelector("#advertising-product-1");
-var body_color_2 = document.querySelector("#advertising-product-2");
-var body_color_3 = document.querySelector("#advertising-product-3");
 var body = document.querySelector("body");
+var body_color_1 = document.querySelector("#advertising-product-1");
+var body_color_1_for_ie = document.querySelector(".button-slider-1");
+var body_color_2 = document.querySelector("#advertising-product-2");
+var body_color_2_for_ie = document.querySelector(".button-slider-2");
+var body_color_3 = document.querySelector("#advertising-product-3");
+var body_color_3_for_ie = document.querySelector(".button-slider-3");
 var open_popup_connection = document.querySelector(".button-connection");
 var overlay = document.querySelector(".overlay");
 var popup_connection = document.querySelector(".modal-connection");
@@ -11,27 +14,14 @@ var name_form_popup_connection = form_popup_connection.querySelector("[name=name
 var email_form_popup_connection = form_popup_connection.querySelector("[name=email]");
 var comment_form_popup_connection = form_popup_connection.querySelector("[name=comment]");
 var button_form_popup_connection = form_popup_connection.querySelector(".button");
-// var storage_name = localStorage ? localStorage.getItem("name") : null; // Зап
-var storage_name = localStorage.getItem("name");
-// var storage_email = localStorage ? localStorage.getItem("email") : null;
-var storage_email = localStorage.getItem("email");
-var is_storage_support = true;
-
-try {
-  storage_name = localStorage.getItem("name");
-  // storage_email = localStorage.getItem("email");
-} catch (err) {
-  is_storage_support = false;
-};
-
-try {
-  // storage_name = localStorage.getItem("name");
-  storage_email = localStorage.getItem("email");
-} catch (err) {
-  is_storage_support = false;
-};
 
 body_color_1.addEventListener("focus", function () {
+  body.classList.add("js-color-1");
+  body.classList.remove("js-color-2");
+  body.classList.remove("js-color-3");
+});
+
+body_color_1_for_ie.addEventListener("click", function () {
   body.classList.add("js-color-1");
   body.classList.remove("js-color-2");
   body.classList.remove("js-color-3");
@@ -43,7 +33,19 @@ body_color_2.addEventListener("focus", function () {
   body.classList.remove("js-color-3");
 });
 
+body_color_2_for_ie.addEventListener("click", function () {
+  body.classList.add("js-color-2");
+  body.classList.remove("js-color-1");
+  body.classList.remove("js-color-3");
+});
+
 body_color_3.addEventListener("focus", function () {
+  body.classList.add("js-color-3");
+  body.classList.remove("js-color-1");
+  body.classList.remove("js-color-2");
+});
+
+body_color_3_for_ie.addEventListener("click", function () {
   body.classList.add("js-color-3");
   body.classList.remove("js-color-1");
   body.classList.remove("js-color-2");
@@ -54,42 +56,37 @@ open_popup_connection.addEventListener("click", function (evt) {
   overlay.classList.add("js-modal-open");
   popup_connection.classList.add("js-modal-open");
   popup_connection.classList.add("js-animation-modal-open");
-  if (is_storage_support) {
-    name_form_popup_connection.value = storage_name;
-    email_form_popup_connection.value = storage_email;
-    comment_form_popup_connection.focus();
-  } else {
-      name_form_popup_connection.focus();
-    };
-  form_popup_connection.addEventListener("submit", function (evt) {
-    if (!name_form_popup_connection.value || !email_form_popup_connection.value || !comment_form_popup_connection.value) {
-      evt.preventDefault();
-      popup_connection.classList.add("js-animation-modal-error");
-    } else {
-      localStorage.setItem("name", name_form_popup_connection.value);
-      localStorage.setItem("email", email_form_popup_connection.value);
-      overlay.classList.remove("js-modal-open");
-      popup_connection.classList.remove("js-modal-open");
-      popup_connection.classList.remove("js-animation-modal-open");
-    };
-  });
-  button_form_popup_connection.addEventListener("click", function () {
-    popup_connection.classList.remove("js-animation-modal-error");
-  });
-  close_popup_connection.addEventListener("click", function (evt) {
+  name_form_popup_connection.focus();
+});
+
+form_popup_connection.addEventListener("submit", function (evt) {
+  if (!name_form_popup_connection.value || !email_form_popup_connection.value || !comment_form_popup_connection.value) {
     evt.preventDefault();
-    overlay.classList.remove("js-modal-open");
-    popup_connection.classList.remove("js-modal-open");
-    popup_connection.classList.remove("js-animation-modal-open");
-    popup_connection.classList.remove("js-animation-modal-error");
-  });
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
+    popup_connection.classList.add("js-animation-modal-error");
+  };
+});
+
+button_form_popup_connection.addEventListener("click", function () {
+  popup_connection.classList.remove("js-animation-modal-open");
+  popup_connection.classList.remove("js-animation-modal-error");
+});
+
+close_popup_connection.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  overlay.classList.remove("js-modal-open");
+  popup_connection.classList.remove("js-modal-open");
+  popup_connection.classList.remove("js-animation-modal-open");
+  popup_connection.classList.remove("js-animation-modal-error");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup_connection.classList.contains("js-modal-open")) {
       evt.preventDefault();
       overlay.classList.remove("js-modal-open");
       popup_connection.classList.remove("js-modal-open");
       popup_connection.classList.remove("js-animation-modal-open");
       popup_connection.classList.remove("js-animation-modal-error");
     };
-  });
+  };
 });
